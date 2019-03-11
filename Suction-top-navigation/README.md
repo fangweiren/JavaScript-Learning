ec8ebffed875b0ed00ac7c62b19482f793638295
fangweiren <fwr843@163.com>
2019-03-11 23:01:30 +0800

    导航吸顶

---
+++ Suction-top-navigation/README.md
@@ -0,0 +1,6 @@
+## 导航吸顶
+
+### 效果展示
+![Suction top navigation](https://github.com/fangweiren/JavaScript-Learning/raw/master/Suction-top-navigation/images/Suction-top-navigation.png)
+
+![Suction top navigation](https://github.com/fangweiren/JavaScript-Learning/raw/master/Suction-top-navigation/images/Suction-top-navigation2.png)
---
+++ Suction-top-navigation/js/myFunc.js
@@ -0,0 +1,27 @@
+/*
+* 获取滚动的头部距离和左边距离
+* scroll().top scroll().left
+* */
+function scroll() {
+    if(window.pageYOffset !== null){
+        return {
+            top: window.pageYOffset,
+            left: window.pageXOffset
+        }
+    }else if(document.compatMode === "CSS1Compat"){ // W3C
+        return {
+            top: document.documentElement.scrollTop,
+            left: document.documentElement.scrollLeft
+        }
+    }
+
+    return {
+        top: document.body.scrollTop,
+        left: document.body.scrollLeft,
+    }
+}
+
+
+function $(id) {
+    return typeof id === "string" ? document.getElementById(id) : null;
+}
